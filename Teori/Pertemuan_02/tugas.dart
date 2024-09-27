4c6b82aa6444101c128d7a2c61ab1ed6b087c732
Rhystalgie <aufamuh12@gmail.com>
2024-09-27 21:44:08 +0700

    revisi jawaban no 2

--- Teori/Pertemuan_02/tugas.dart
+++ Teori/Pertemuan_02/tugas.dart
@@ -21,14 +21,10 @@ void main() {
 
 // 1. Buatkan algoritma
 // a. Menghitung rerata dari IPK
-  double totalIPK = 0;
-  for (int i = 0; i < daftarMahasiswa.length; i++) {
-    totalIPK += daftarMahasiswa[i]["ipk"];
-  }
-
-  var rerataIPK = totalIPK / daftarMahasiswa.length;
-  print("Rerata IPK Mahasiswa adalah " + rerataIPK.toString());
 // b. Menjumlahkan berapa banyak mahasiswa yang sudah nikah
+
+// 2. Masing-masing dari poin 1.a dan 1.b diimplementasikan dalam kode program
+// 2.a
   var mahasiswaNikah = 0;
   for (int i = 0; i < daftarMahasiswa.length; i++) {
     if (daftarMahasiswa[i]["isMarried"] == true) {
@@ -38,5 +34,12 @@ void main() {
 
   print("Jumlah mahasiswa yang telah menikah adalah " +
       mahasiswaNikah.toString());
-// 2. Masing-masing dari poin 1.a dan 1.b diimplementasikan dalam kode program
+// 2.b
+  double totalIPK = 0;
+  for (int i = 0; i < daftarMahasiswa.length; i++) {
+    totalIPK += daftarMahasiswa[i]["ipk"];
+  }
+
+  var rerataIPK = totalIPK / daftarMahasiswa.length;
+  print("Rerata IPK Mahasiswa adalah " + rerataIPK.toString());
 }
