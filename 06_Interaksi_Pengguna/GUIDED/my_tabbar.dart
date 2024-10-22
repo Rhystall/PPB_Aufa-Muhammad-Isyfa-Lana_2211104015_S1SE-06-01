3d5f82d952d028748d22f0ba59bf27dc5c6c6ec4
Rhystalgie <aufamuh12@gmail.com>
2024-10-22 23:14:32 +0700

    SS + GUIDED Code

---
+++ 06_Interaksi_Pengguna/GUIDED/my_tabbar.dart
@@ -0,0 +1,94 @@
+// import 'package:flutter/material.dart';
+
+// class MyTabBarScreen extends StatelessWidget {
+//   const MyTabBarScreen({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return DefaultTabController(
+//       length: 3, // Number of tabs
+//       child: Scaffold(
+//         appBar: AppBar(
+//           title: Text("Belajar Tab Bar"),
+//           backgroundColor: Colors.yellow,
+//           bottom: TabBar(
+//             tabs: [
+//               Tab(
+//                 icon: Icon(Icons.home),
+//                 text: "Home",
+//               ),
+//               Tab(
+//                 icon: Icon(Icons.person),
+//                 text: "Profile",
+//               ),
+//               Tab(
+//                 icon: Icon(Icons.settings),
+//                 text: "Settings",
+//               ),
+//             ],
+//           ),
+//         ),
+//         body: TabBarView(
+//           children: [
+//             Center(
+//               child: Text("Home"),
+//             ),
+//             Center(
+//               child: Text("Profile"),
+//             ),
+//             Center(
+//               child: Text("Settings"),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+// import 'package:flutter/material.dart';
+
+// class MyTabBarScreen extends StatelessWidget {
+//   const MyTabBarScreen({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return DefaultTabController(
+//       length: 3, // Number of tabs
+//       child: Scaffold(
+//         appBar: AppBar(
+//           title: Text("Belajar Tab Bar"),
+//           backgroundColor: Colors.yellow,
+//           bottom: TabBar(
+//             tabs: [
+//               Tab(
+//                 icon: Icon(Icons.home),
+//                 text: "Home",
+//               ),
+//               Tab(
+//                 icon: Icon(Icons.person),
+//                 text: "Profile",
+//               ),
+//               Tab(
+//                 icon: Icon(Icons.settings),
+//                 text: "Settings",
+//               ),
+//             ],
+//           ),
+//         ),
+//         body: TabBarView(
+//           children: [
+//             Center(
+//               child: Text("Home"),
+//             ),
+//             Center(
+//               child: Text("Profile"),
+//             ),
+//             Center(
+//               child: Text("Settings"),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
