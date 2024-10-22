0bcc4daa09dbe30cd3b94029113644fe5fd9f285
Rhystalgie <aufamuh12@gmail.com>
2024-10-22 23:06:00 +0700

    main.dart TP & UNGUIDED

---
+++ 06_Interaksi_Pengguna/GUIDED/my_package.dart
@@ -0,0 +1,53 @@
+// import 'package:flutter/material.dart';
+// import 'package:google_fonts/google_fonts.dart';
+
+// class MyPackageScreen extends StatelessWidget {
+//   const MyPackageScreen({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text(
+//           "Belajar Package",
+//           style: GoogleFonts.poppins(color: Colors.white),
+//         ),
+//         backgroundColor: Colors.black,
+//       ),
+//       body: Padding(
+//         padding: EdgeInsets.all(8.0),
+//         child: Column(
+//           children: [
+//             TextField(
+//               keyboardType: TextInputType.text,
+//               decoration: InputDecoration(
+//                   hintText: "Masukkan Email",
+//                   border: OutlineInputBorder(),
+//                   prefixIcon: Icon(Icons.person)),
+//             ),
+//             SizedBox(
+//               height: 10,
+//             ),
+//             TextField(
+//               keyboardType: TextInputType.text,
+//               decoration: InputDecoration(
+//                   hintText: "Masukkan Password",
+//                   border: OutlineInputBorder(),
+//                   prefixIcon: Icon(Icons.lock)),
+//             ),
+//             SizedBox(height: 10),
+//             ElevatedButton(
+//               onPressed: () {},
+//               child: Text("Ini adalah Elevated Button"),
+//             ),
+//             SizedBox(height: 10),
+//             TextButton(
+//               onPressed: () {},
+//               child: Text("Ini adalah Text Button"),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
---
+++ 06_Interaksi_Pengguna/TP/main.dart
@@ -0,0 +1,102 @@
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter Demo',
+//       theme: ThemeData(
+//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+//         useMaterial3: true,
+//       ),
+//       home: const MyHomePage(),
+//       debugShowCheckedModeBanner: false,
+//     );
+//   }
+// }
+
+// class MyHomePage extends StatefulWidget {
+//   const MyHomePage({super.key});
+
+//   @override
+//   State<MyHomePage> createState() => _MyHomePageState();
+// }
+
+// class _MyHomePageState extends State<MyHomePage> {
+//   int _currentIndex = 0;
+//   Widget _getSelectedPage(int index) {
+//     switch (index) {
+//       case 0:
+//         return const Center(
+//           child: Text(
+//             "Isi Halaman Beranda",
+//             style: TextStyle(
+//               fontSize: 16,
+//             ),
+//           ),
+//         );
+//       case 1:
+//         return const Center(
+//           child: Text(
+//             "Isi Halaman Wisata",
+//             style: TextStyle(
+//               fontSize: 16,
+//             ),
+//           ),
+//         );
+//       case 2:
+//         return const Center(
+//           child: Text(
+//             "Isi Halaman Profile",
+//             style: TextStyle(
+//               fontSize: 16,
+//             ),
+//           ),
+//         );
+//       default:
+//         return const Center(
+//           child: Text("Halaman tidak ditemukan"),
+//         );
+//     }
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text("Latihan BottomNavigationBar"),
+//         backgroundColor: Colors.amber,
+//       ),
+//       body: _getSelectedPage(_currentIndex),
+//       bottomNavigationBar: BottomNavigationBar(
+//         selectedItemColor: Colors.amber,
+//         currentIndex: _currentIndex,
+//         onTap: (index) {
+//           setState(() {
+//             _currentIndex = index;
+//           });
+//         },
+//         items: const [
+//           BottomNavigationBarItem(
+//             icon: Icon(Icons.home),
+//             label: "Home",
+//           ),
+//           BottomNavigationBarItem(
+//             icon: Icon(Icons.business),
+//             label: "Wisata",
+//           ),
+//           BottomNavigationBarItem(
+//             icon: Icon(Icons.person),
+//             label: "Profile",
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
