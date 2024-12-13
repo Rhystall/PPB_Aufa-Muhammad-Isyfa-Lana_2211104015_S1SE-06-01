f785eda069976dade61741afcc5b4cc33d9618d3
Rhystalgie <aufamuh12@gmail.com>
2024-12-13 18:30:54 +0700

    upload file GUIDED

---
+++ 13_Networking/GUIDED/main.dart
@@ -0,0 +1,25 @@
+// import 'package:belajar_statemanagement/detail_page.dart';
+// import 'package:flutter/material.dart';
+// import 'package:get/get.dart';
+// import 'my_home_page.dart'; // Import file MyHomePage Anda
+
+// void main() {
+//   runApp(MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return GetMaterialApp(
+//       initialRoute: '/',
+//       getPages: [
+//         GetPage(name: '/', page: () => MyHomePage()), // Halaman utama
+//         GetPage(name: '/details', page: () => DetailsPage()), // Halaman detail
+//       ],
+//       theme: ThemeData(
+//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+//         useMaterial3: true,
+//       ),
+//     );
+//   }
+// }
